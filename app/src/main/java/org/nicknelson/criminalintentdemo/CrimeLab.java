f60543f7d5d67f77590b35fd5ddb87718c739321
Nick Desktop <nicknelson1021@gmail.com>
2019-01-13 15:19:44 -0600

    adding SQLite Database

--- app/src/main/java/org/nicknelson/criminalintentdemo/CrimeLab.java
+++ app/src/main/java/org/nicknelson/criminalintentdemo/CrimeLab.java
@@ -1,6 +1,9 @@
 package org.nicknelson.criminalintentdemo;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import org.nicknelson.criminalintentdemo.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +12,10 @@ import java.util.UUID;
 public class CrimeLab {
 
     private static CrimeLab sCrimeLab;
+
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
 
@@ -21,6 +27,8 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
         mCrimes = new ArrayList<>();
 
         /*
